e15cb771a1cbdf02f5e2cbd9d8ad405f411286bb
Paige <fredepai@umich.edu>
2017-10-08 13:54:39 -0700

    Added toggle buttons

---
+++ Assets/DisplayImg.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayImg : MonoBehaviour {
+
+	public GameObject Profile;
+
+	// Use this for initialization
+	void Start () {
+		Profile.SetActive(false);
+	}
+
+	public void OnSubmit () {
+		Profile.SetActive (true);
+	}
+}
---
+++ Assets/ShowPatientInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowPatientInfo : MonoBehaviour {
+
+	public Image img;
+
+	// Use this for initialization
+	public void ToggleChanged (bool value) {
+		img.enabled = value;
+	}
+	
+	// Update is called once per frame
+
+}
